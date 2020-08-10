4f8a5bf8504541a1d533f7655742a371cf19fdbc
amaralc <amaral.calil@gmail.com>
2020-08-09 22:16:53 -0300

    FEATURE / Add POST logic

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,10 +11,29 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  // Destructure title, url and techs from request.body
+  const {title, url, techs} = request.body;
+
+  // Create project object
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,  
+    likes: 0
+  }
+
+  // Add project to list of repositories
+  repositories.push(project);
+
+  // Send response with status 200 and project data
+  response.status(200).json(project);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
